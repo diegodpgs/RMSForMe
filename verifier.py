ec5bfda634342d387f89e92f0086233a956462e0
Diego Pedro Gonçalves <diego@Diegos-MacBook-Air.local>
2017-01-20 17:53:13 -0300

    added srt and verified.py modified

--- verifier.py
+++ verifier.py
@@ -10,8 +10,9 @@ def init_verify():
 	watchlist_data = open(FILE_WATCHLIST_DATA).read().split('\n')
 	
 	movies_name = []
-	movies_name.extend([(i.split(';')[0],i.split(';')[1]) for i in watchlist_data])
-	movies_name.extend([(w.split(';')[1],w.split(';')[0]) for w in watched_data])
+	movies_name.extend([(i.split(';')[0],[i.split(';')[1],int(i.split(';')[2])]) for i in watchlist_data])
+	
+	movies_name.extend([(i.split(';')[0],[i.split(';')[1],int(i.split(';')[2])]) for i in watched_data])
 	movies_name = dict(movies_name)
 
 	#****---------------    SRT   ---------------****#
@@ -38,13 +39,13 @@ def init_verify():
 		train_tag = train_tag[1:]
 	train_tag = ['tt'+i.split('_tt')[1].split('.')[0] for i in train_tag]
 
-
 	verify_movies_data(watched_imdb,watchlist_imdb,watched_data,watchlist_data)
 	verify_srt(train_srt,test_srt,watchlist_imdb,watched_imdb,movies_name)
 	verify_tag(train_tag,test_tag,watchlist_imdb,watched_imdb,movies_name)
 	#verify_utterance()
 
 def verify_srt(srt_train,srt_test,new_movies_list,watched_movies_list,dic_movies):
+	
 	"""
 		srt_train,srt_test,watched_movies_list,new_movies:  [list of IMDB code]
 		dic_movies: a dicionary that maps IMDB_code to movie title
@@ -62,42 +63,62 @@ def verify_srt(srt_train,srt_test,new_movies_list,watched_movies_list,dic_movies
 	"""
 
 	for srt_imdb_code in srt_test:
-		if srt_imdb_code in new_movies_list and srt_imdb_code in watched_movies_list:
-			print '%s/%s was already watched. It was removed from new movie list' % (dic_movies[srt_imdb_code],srt_imdb_code)
+
+		if srt_imdb_code not in dic_movies:
+			print '%s is not within watchlist.imdb and not in watched.imdb, but in srt/test' % srt_imdb_code
+			continue
+		movie_title = dic_movies[srt_imdb_code][0]
+
+		if not (1930<= dic_movies[srt_imdb_code][1] <= 2015):
+			print '%s is not considered for srt because it was released on %d' % (movie_title,dic_movies[srt_imdb_code][1])
+
+		elif srt_imdb_code in new_movies_list and srt_imdb_code in watched_movies_list:
+			print '%s/%s was already watched. It was removed from new movie list' % (movie_title,srt_imdb_code)
 			new_movies_list.remove(srt_imdb_code)
 
 		elif srt_imdb_code not in new_movies_list and srt_imdb_code not in watched_movies_list:
-			print '%s/%s is not in new movies list and watched movies list' % (dic_movies[srt_imdb_code],srt_imdb_code)
+			print '%s/%s is not in new movies list and watched movies list' % (movie_title,srt_imdb_code)
 
 		elif srt_imdb_code in srt_train:
-			print '%s/%s/test must to be removed from TEST/database' % (dic_movies[srt_imdb_code],srt_imdb_code) 
+			print '%s/%s/test must to be removed from TEST/database' % (movie_title,srt_imdb_code) 
 
 	for srt_imdb_code in srt_train:
+
+		if srt_imdb_code not in dic_movies:
+			print '%s is not within watchlist.imdb and not in watched.imdb, but in srt/train' % srt_imdb_code
+			continue
 		
 		if srt_imdb_code in new_movies_list and srt_imdb_code in watched_movies_list:
-			print '%s/%s was already watched. It was removed from new movie list' % (dic_movies[srt_imdb_code],srt_imdb_code)
+			print '%s/%s was already watched. It was removed from new movie list' % (dic_movies[srt_imdb_code][0],srt_imdb_code)
 			new_movies_list.remove(srt_imdb_code)
 
 		elif srt_imdb_code not in new_movies_list and srt_imdb_code not in watched_movies_list:
-			print '%s/%s is not in new movies list and watched movies list' % (dic_movies[srt_imdb_code],srt_imdb_code)
+			print '%s/%s is not in new movies list and watched movies list' % (dic_movies[srt_imdb_code][0],srt_imdb_code)
 
 
+	
 
 	for imdb_code in new_movies_list:
+
 		if imdb_code not in srt_test:
 			if imdb_code not in dic_movies:
 				print '%s srt file must to be Downloaded to TEST' % (imdb_code)
 			else:
-				print '%s/%s srt file must to be Downloaded to TEST' % (dic_movies[imdb_code],imdb_code)
+				if not (1930<= dic_movies[imdb_code][1] <= 2015):
+					print '%s is not considered for srt because it was released on %d' % (dic_movies[imdb_code][0],dic_movies[imdb_code][1])
+					continue
 
-	for code_movie in watched_movies_list:
-		if code_movie not in srt_train and code_movie not in srt_test:
+				else:
+					print '%s/%s srt file must to be Downloaded to TEST' % (dic_movies[imdb_code][0],imdb_code)
+
+	for imdb_code in watched_movies_list:
+		if imdb_code not in srt_train and imdb_code not in srt_test:
 			if imdb_code not in dic_movies:
 				print '%s srt file must to be Downloaded to TRAIN' % (imdb_code)
 			else:
-				print '%s/%s srt file must to be Downloaded to TRAIN' % (dic_movies[imdb_code],imdb_code)
-		elif code_movie not in srt_train:
-			print '%s/%s must to be copied from srt_test to TRAIN' % (dic_movies[imdb_code],imdb_code)
+				print '%s/%s srt file must to be Downloaded to TRAIN' % (dic_movies[imdb_code][0],imdb_code)
+		elif imdb_code not in srt_train:
+			print '%s/%s must to be copied from srt_test to TRAIN' % (dic_movies[imdb_code][0],imdb_code)
 
 	fnew_movie_titles = open(FILE_WATCHLIST_IMDB,'w')
 
@@ -123,14 +144,14 @@ def verify_movies_data(watched_imdb,watchlist_imdb,watched_data,watchlist_data):
 		f = nltk.FreqDist(watchlist_imdb)
 		for i in f.most_common(50):
 			if i[1] > 1:
-				print '----',i[0], 'was duplicated',i[1],'times'
+				print '----',i[0], 'was duplicated',i[1],'times in watchlist.imdb'
 
 	if len(watched_imdb) != len(set(watched_imdb)):
 		print 'There are register duplicated in watched.imdb'
 		f = nltk.FreqDist(watched_imdb)
 		for i in f.most_common(50):
 			if i[1] > 1:
-				print '----',i[0], 'was duplicated',i[1],'times'
+				print '----',i[0], 'was duplicated',i[1],'times in watched.imdb'
 		
 
 	if len(watchlist_data) != len(set(watchlist_data)):
@@ -162,9 +183,10 @@ def verify_movies_data(watched_imdb,watchlist_imdb,watched_data,watchlist_data):
 
 def verify_tag(tag_train,tag_test,new_movies_list,watched_movies_list,dic_movies):
 
+
 	for imdb_code in tag_test:
 		if imdb_code not in new_movies_list and imdb_code in watched_movies_list:
-			print 'Move %s/%s from test to TAG_train_folder' % (imdb_code,dic_movies[imdb_code])
+			print 'Move %s/%s from test to TAG_train_folder' % (imdb_code,dic_movies[imdb_code][0])
 
 	for imdb_code in new_movies_list:
 		if imdb_code not in tag_test and imdb_code not in watched_movies_list:
